d0f9529f4710133415b48ca6903387dde0a4443e
Taehee <klimes888@gmail.com>
2021-09-11 17:27:30 +0900

    cors fixing

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,23 @@
-  import * as express from 'express';
+  
+  import express, {Application, Request, Response} from 'express';
+  import cors from 'cors'
   // import postRouter from './routes/post';
 
-  const app: express.Application = express();
+  const app: Application = express();
+  app.use(express.json())
 
+  const corsOptions = {
+    origin: 'http://localhost:8080', // 허락하고자 하는 요청 주소
+    credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+};
+
+  app.use(cors(corsOptions))
   // get
-  app.get('/', (req: express.Request, res: express.Response) => {
+  app.get('/', (req: Request, res: Response) => {
     res.send('hello express');
   });
 
-  app.get('/posts', (req: express.Request, res: express.Response) => {
+  app.get('/posts', (req: Request, res: Response) => {
     res.json([
       { id: 1, content: 'hello' },
       { id: 2, content: 'hello2' },
@@ -18,6 +27,6 @@
 
   // app.use('/post', postRouter);
 
-  app.listen(3000, () => {
+  app.listen(3000, '0,0,0,0,', () => {
     console.log('실행중');
   });
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {jsx, css} from '@emotion/react';
+import axios from 'axios';
 
 export default function Main() {
+
+    const fetch = () => {
+        axios({
+            method:'get',
+            url:'/posts',
+        }).then((test) => console.log(test))
+        .catch( err => console.log(err))
+    }
+
+    useEffect(() => {
+        fetch();
+    }, [])
 
 
     return (
         <div css={mainStyle}>
-            test
+            testdsd
         </div>
     )
 }
