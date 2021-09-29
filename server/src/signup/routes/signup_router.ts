e84ff61bc7fe7750105584c25502e19185cb1feb
Taehee <klimes888@gmail.com>
2021-09-29 18:19:07 +0900

    server bcrypt add

--- server/src/signup/routes/signup_router.ts
+++ server/src/signup/routes/signup_router.ts
@@ -1,6 +1,6 @@
 import express, {Application, Request, Response} from 'express';
 let mysql = require('mysql2');
-
+const bcrypt = require('bcrypt');
 const db = mysql.createConnection({
     host:'localhost',
     user:'taehee',
@@ -13,18 +13,32 @@ db.connect()
 
 const router = express.Router();
 
-router.post('/', function(req, res, next) {
+router.post('/', function(req, res) {
     const {email, password, repassword, name} = req.body;
     const param = [email, password, name]
-    if(email && password){
-        if(password !== repassword) return;
-        db.query(`INSERT INTO users('email', 'password', 'name') VALUES(?, ?, ?), ${param}`, (err:Error, row:any) => {
-            if(err){ console.log(err); return;};
-            console.log(row);
-            res.send('api 정상 작동');
-        })
+    let result:{} = {}
+    if(email && password && name){
+        if(password === repassword){
+            console.log(param);
+            bcrypt.hash(param[1], 10, (err:Error, hash:any) => {
+                param[1] = hash
+                db.query('INSERT INTO users(`email`, `password`, `name`) VALUES(?, ?, ?)', param, (err:Error, row:any) => {
+                    
+                    if(err){
+                        result = {...result, status:500, message:'db error fail'}
+                        console.log('db query error', err);
+                        return;
+                    };
+                    result = {...result, status:200, message:'success', data:{token:''} }
+                })
+            })
+        } else {
+            console.log('비밀번호 안맞음');
+            result = {...result, status:500, message:'password fail', data:{token:''} }
+
+        }
     }
-    // res.send('api 정상 작동');
+    res.send(result);
     res.end();
     // res.json({ title: req });
 });
